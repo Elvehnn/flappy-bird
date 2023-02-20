b3e3ff160344fa9ce863eb8df5e26e98541727ed
Elena Shashina <elvehnn@yandex.ru>
2023-02-20 13:36:58 +0300

    feat: change some styles

--- packages/client/src/components/Preloader/Preloader.tsx
+++ packages/client/src/components/Preloader/Preloader.tsx
@@ -4,6 +4,7 @@ import {
     LOADING_TITLE,
     PRESENTATION_IMAGE_DARK,
 } from "@/constants/imagesPaths";
+
 const { Footer } = Layout;
 
 const Preloader = () => {
@@ -12,10 +13,13 @@ const Preloader = () => {
             className="layout"
             data-testid="loader-page"
             style={{
+                margin: "0px",
+                padding: "0px",
                 display: "flex",
                 flexDirection: "column",
                 alignItems: "center",
-                justifyContent: "space-between",
+                justifyContent: "center",
+                rowGap: 20,
                 backgroundImage: `url(${PRESENTATION_IMAGE_DARK})`,
                 backgroundSize: "cover",
             }}>
@@ -26,6 +30,7 @@ const Preloader = () => {
                     flexDirection: "column",
                     alignItems: "center",
                     justifyContent: "center",
+                    rowGap: 20,
                 }}>
                 <Image width={100} src={COLORED_LOGO} data-testid="logo" />
                 <Image
